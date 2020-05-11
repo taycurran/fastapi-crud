ce5fb6652c2ba5c0043c1057fe793c7ac8a9d122
taycurran <taylorcurranagtc@gmail.com>
2020-05-11 13:25:44 -0500

    Creating DB Model

--- src/app/db.py
+++ src/app/db.py
@@ -14,4 +14,28 @@ engine = create_engine(DATABASE_URL)
 metadata = MetaData()
 
 # We create a new Database instance from databases.
+
+prices = Table(
+    "prices",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("source", String(200)),
+    Column("country", String(5)),
+    Column("market", String(25)),
+    Column("product_cat", String(50)),
+    Column("product_agg", String(50)),
+    Column("product", String(50)),
+    Column("date", Datetime,),
+    Column("retail", Integer),
+    Column("wholesale", Integer),
+    Column("currency", String(5)),
+    Column("unit", String(5)),
+    Column("active", String(5)),
+    Column("udate", DateTime, default=func.now(), nullable=False),
+    )
+
 database = Database(DATABASE_URL)
+
+
+
+
